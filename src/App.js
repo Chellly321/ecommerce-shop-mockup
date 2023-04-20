a56e100ffcc296ac33a405aaf7980b6bdcfb621d
Chelly321 <michelle.royer@icloud.com>
2023-04-20 10:41:33 -0400

    checkout+routerFix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Home from "./Home";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+
+        <Switch>
+
+          <Route exact path="/">    
+            <Header />
+            <Home />
+          </Route>
+
+        <Route exact path="/checkout">
+            <Header />
+            <Checkout />
+        </Route>
+          
+        </Switch>
+        
+      </Router>
+
     </div>
   );
 }
